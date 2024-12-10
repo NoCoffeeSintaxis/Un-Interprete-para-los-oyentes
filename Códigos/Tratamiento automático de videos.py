9dfb624936fcd7c05a078ede8d9e730b5e810f6b
ClaudiaRangel22 <claudia2220665@correo.uis.edu.co>
2024-12-09 22:19:16 -0500

    Se subieron los códigos para el tratamiento automático de matrices de datos.

--- Códigos/Tratamiento automático de videos.py
+++ Códigos/Tratamiento automático de videos.py
@@ -1,9 +1,7 @@
 import numpy as np
 import cv2
 import os
-import string  # Para trabajar con las letras del abecedario
 
-<<<<<<< HEAD
 ## Este código pasa por cada video de cada carpeta de la carpeta Videos, redimensiona los videos, 
     # escoge solo 5 frames por video y los guarda en una matriz numpy en la carpeta "Datos" con el 
     # mismo nombre del video.
@@ -32,31 +30,12 @@ for letra in abc:
     videos_1_3 = [list_name_videos[0], list_name_videos[11], list_name_videos[20]]
 
     # Iterar sobre los archivos en la carpeta
-=======
-# Ruta base de los videos
-videos_base_path = 'C:/Users/juand/Desktop/Un-Interprete-para-los-oyentes/Videos'
-frame_lapse = 5
-
-# Ruta base de guardado
-datos_base_path = 'C:/Users/juand/Desktop/Un-Interprete-para-los-oyentes/Datos'
-
-# Iterar sobre las letras del abecedario
-for letter in string.ascii_lowercase:  # Letras en Minúscula
-    folder_path = os.path.join(videos_base_path, letter)
-    
-    # Creación de la carpeta destino
-    output_dir = os.path.join(datos_base_path, letter.upper()) 
-    os.makedirs(output_dir, exist_ok=True)
-
-    # Procesamiento
->>>>>>> 4ca6982dfa6e4f013714e0a1fb8a188df9737ee9
     for filename in os.listdir(folder_path):
         if filename.endswith('.mp4'):  
             # Inicializar una lista para almacenar los frames
             frames = []
             
             # Cargar el video
-<<<<<<< HEAD
             cap = cv2.VideoCapture(os.path.join(folder_path, filename))
             name = os.path.splitext(filename)[0]  # Eliminar la extensión del archivo
             dirc = os.path.join(output_dir, name + '.npy')  # Crear la ruta completa con nombre de archivo
@@ -118,35 +97,3 @@ for letter in string.ascii_lowercase:  # Letras en Minúscula
 
 #Printear número de errores
 print(f'Número de errores: {errores}')
-=======
-            video_path = os.path.join(folder_path, filename)
-            cap = cv2.VideoCapture(video_path)
-            name = os.path.splitext(filename)[0] 
-            output_file_path = os.path.join(output_dir, name + '.npy') 
-
-            # Leer frames según el intervalo definido
-            for i in range(0, int(cap.get(cv2.CAP_PROP_FRAME_COUNT)), frame_lapse):
-                cap.set(cv2.CAP_PROP_POS_FRAMES, i) 
-                ret, frame = cap.read() 
-                
-                if not ret:
-                    break
-                
-                # Redimensionar y convertir el frame a escala de grises
-                frame = cv2.resize(frame, (474, 850)) #Estos valores se proponen del análisis dado por "Análisis de dimensiones"
-                gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                
-                frames.append(gray_frame) #Agregar frame ya procesado a la lista
-
-            # Convertir la lista de frames a una matriz tridimensional
-            A = np.stack(frames, axis=2) if frames else None
-            
-            # Guardar la matriz en un archivo .npy
-            if A is not None:
-                np.save(output_file_path, A)
-                print(f"Matriz guardada: {output_file_path}")
-            else:
-                print(f"No se generó matriz para el video {video_path}.")
-            
-            cap.release()
->>>>>>> 4ca6982dfa6e4f013714e0a1fb8a188df9737ee9
